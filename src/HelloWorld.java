740a2997bc28f6256c3e3965913ab0d1e6cb8ac4
1N3 computer <bths@sample.com>
2025-11-20 09:57:51 -0500

    added some print statements

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        System.out.println("hello world 123!");
+        System.out.println("this is cool");
+        System.out.println("this is cool!!!");
     }
 }
